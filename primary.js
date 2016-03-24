e92567d63d9ff7f770d18224586d48db997df5f0
cmayeda <cassidymayeda@gmail.com>
2016-03-23 20:53:44 -0400

    arrow keys working

--- primary.js
+++ primary.js
@@ -48,59 +48,75 @@ var userPlay = function(direction) {
 
 		} else if(direction == "down" && i+4 < 16) {
 			
-			var changeBOX = document.getElementsByClassName('box')[i+4];
-			var changeBOXList = changeBOX.className.split(" ");
+			switch(i) { 
+				case 12: 
+					break;
+				case 13:
+					break;
+				case 14: 
+					break;
+				case 16: 
+					break;
+				default: 
+					var changeBOX = document.getElementsByClassName('box')[i+4];
+					var changeBOXList = changeBOX.className.split(" ");
 
-			if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
+					if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
 
-				var color = currentClassList[1];
-				changeBOX.classList.add(color);
-				BOX.classList.remove(color);
-				//i++;
-			} 
+						var color = currentClassList[1];
+						changeBOX.classList.add(color);
+						BOX.classList.remove(color);
+					}		
+			}
 
 		//might still need to change back to if/else for second condition 	
 		} else if(direction == "left" && i > 0) {
  
+			switch(i) { 
+				case 0: 
+					break;
+				case 4:
+					break;
+				case 8: 
+					break;
+				case 12:
+					break;
+				default: 
+					var changeBOX = document.getElementsByClassName('box')[i-1];
+					var changeBOXList = changeBOX.className.split(" ");
 
+					if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
 
+						var color = currentClassList[1];
+						changeBOX.classList.add(color);
+						BOX.classList.remove(color);
 
-
-
-
-
-
- 
-				var changeBOX = document.getElementsByClassName('box')[i-1];
-				var changeBOXList = changeBOX.className.split(" ");
-
-				if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
-
-					var color = currentClassList[1];
-					changeBOX.classList.add(color);
-					BOX.classList.remove(color);
-
-				}
+					}		
+			}
  
 		} else if(direction == "right") {
 
-			if(i == 3, 7, 11, 15) { 
-				//do nothing
-			} else { 
-				var changeBOX = document.getElementsByClassName('box')[i+1];
-				var changeBOXList = changeBOX.className.split(" ");
+			switch(i) { 
+				case 3:
+					break;
+				case 7:
+					break;
+				case 11: 
+					break;
+				case 16: 
+					break;
+				default: 				
+					var changeBOX = document.getElementsByClassName('box')[i+1];
+					var changeBOXList = changeBOX.className.split(" ");
 
-				if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
+					if(currentClassList.length >= 2 && changeBOXList.length <= 1) { 
 
-					var color = currentClassList[1];
-					changeBOX.classList.add(color);
-					BOX.classList.remove(color);
-
-				}
-			}
-
- 
-			
+						var color = currentClassList[1];
+						changeBOX.classList.add(color);
+						BOX.classList.remove(color);
+					}
+				
+			}		
 
 		} else { 
 			//? 
