0a3309a691a04b3fa226db9959d1b2b8d9413ab7
cmayeda <cassidymayeda@gmail.com>
2016-04-13 18:38:47 -0400

    tried to fix keypresses, added credits

--- createGrid.js
+++ createGrid.js
@@ -29,8 +29,7 @@ var fillGrid = function() {
 
 var addBox = function() { 
 
-	var list = [0, 1, 2, 3, 4, 5, 7, 8, 11, 12, 13, 14, 16];
-	var num = list[Math.floor(Math.random()*13)];
+	var num = Math.floor(Math.random()*15);
 	var BOX = document.getElementsByClassName('box')[num];
 
 	if(BOX.classList.length >= 2) { 
--- primary.js
+++ primary.js
@@ -106,76 +106,95 @@ var changeColor = function(BOX, changeBOX) {
 
 
 		if(currentClassList.length == 2 && changeBOXList.length == 1) { 
+
 				var color = currentClassList[1];
 				changeBOX.classList.add(color);
 				BOX.classList.remove(color);
 
-		//color combinations : primary colors 		
-		} else if(BOXColor == "red" && changeColor == "blue") { //red + blue = purple
-			changeBOX.classList.add("purple"); //works 
-			changeBOX.classList.remove(changeColor);
-			BOX.classList.remove(BOXColor);
-		} else if(BOXColor == "blue" && changeColor == "red") { 
-			changeBOX.classList.add("purple");
-			changeBOX.classList.remove(changeColor);
-			BOX.classList.remove(BOXColor);
-		} else if(BOXColor == "blue" && changeColor == "yellow") { //blue + yellow = green 
-			changeBOX.classList.add("green");
-			changeBOX.classList.remove(changeColor);
-			BOX.classList.remove(BOXColor);
-		} else if(BOXColor == "yellow" && changeColor == "blue") { 
-			changeBOX.classList.add("green");
-			changeBOX.classList.remove("blue");
-			BOX.classList.remove("yellow");
-		} else if(BOXColor == "red" && changeColor == "yellow") { //red + yellow = orange 
-			changeBOX.classList.add("orange"); 
-			changeBOX.classList.remove("yellow");
-			BOX.classList.remove("red");
-		} else if(BOXColor == "yellow" && changeColor == "red") { 
-			changeBOX.classList.add("orange"); 
-			changeBOX.classList.remove("red");
-			BOX.classList.remove("yellow");
+		} else if(currentClassList.length == 2 && changeBOXList.length == 2) { 
+			
+			//color combinations : primary colors 
+	 		if(BOXColor == "red" && changeColor == "blue") { //red + blue = purple
+				changeBOX.classList.add("purple"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "blue" && changeColor == "red") { 
+				changeBOX.classList.add("purple");
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "blue" && changeColor == "yellow") { //blue + yellow = green 
+				changeBOX.classList.add("green");
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "yellow" && changeColor == "blue") { 
+				changeBOX.classList.add("green");
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "red" && changeColor == "yellow") { //red + yellow = orange 
+				changeBOX.classList.add("orange"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "yellow" && changeColor == "red") { 
+				changeBOX.classList.add("orange"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+	
+			//color combinations : 2ndary to brown 	
+			} else if(BOXColor == "purple" && changeColor == "green") { //purple + green = brown 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "green" && changeColor == "purple") { 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "green" && changeColor == "orange") { //green + orange = brown 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "orange" && changeColor == "green") { 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "orange" && changeColor == "purple") { //orange + purple = brown 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "purple" && changeColor == "orange") { 
+				changeBOX.classList.add("brown"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
 
-		//color combinations : 2ndary to brown 	
-		} else if(BOXColor == "purple" && changeColor == "green") { //purple + green = brown 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("green");
-			BOX.classList.remove("purple");
-		} else if(BOXColor == "green" && changeColor == "purple") { 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("purple");
-			BOX.classList.remove("green");
-		} else if(BOXColor == "green" && changeColor == "orange") { //green + orange = brown 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("orange");
-			BOX.classList.remove("green");
-		} else if(BOXColor == "orange" && changeColor == "green") { 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("green");
-			BOX.classList.remove("orange");		
-		} else if(BOXColor == "orange" && changeColor == "purple") { //orange + purple = brown 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("purple");
-			BOX.classList.remove("orange");	
-		} else if(BOXColor == "purple" && changeColor == "orange") { 
-			changeBOX.classList.add("brown"); 
-			changeBOX.classList.remove("orange");
-			BOX.classList.remove("purple");	
+			//color combinations : final 	
+			} else if(BOXColor == "brown" && changeColor == "brown") { 
+				changeBOX.classList.add("black"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "black" && changeColor == "black") { 
+				changeBOX.classList.add("white"); 
+				changeBOX.classList.remove(changeColor);
+				BOX.classList.remove(BOXColor);
+			} else if(BOXColor == "white" && changeColor == "white") { 
 
-		//color combinations : final 	
-		} else if(BOXColor == "brown" && changeColor == "brown") { 
-			changeBOX.classList.add("black"); 
-			changeBOX.classList.remove("brown");
-			BOX.classList.remove("brown");
-		} else if(BOXColor == "black" && changeColor == "black") { 
-			changeBOX.classList.add("white"); 
-			changeBOX.classList.remove("black");
-			BOX.classList.remove("black");
-		} else if(BOXColor == "white" && changeColor == "white") { 
-			BOX.classList.remove("white");
-		} else { 
-			//nothing? 
-		} 
+				BOX.classList.remove("white");
+				//something to cumulate a score? 
+				
+			} else { 
+				//error, should not happen 
+			}
+
+		}
+
+
+
+
+
+
+		
+
+	
+
+
 
 
 }
